9040e291bc92979b33ceea774d143d469af5b7eb
Luis Corales <luisaugustocorales@hotmail.com>
2023-05-30 16:13:23 -0500

    Complete

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { removeStringsByLength } from "./removeStringsByLength";
+
+const list = ["Hola", "viva", "el furbo"];
+
+const myList = removeStringsByLength(list, 5);
+
+console.log(myList);
