992d12e96ca65be773bd918d82ad363053acc736
Luis Corales <luisaugustocorales@hotmail.com>
2023-05-31 22:17:26 -0500

    step1

--- removeStringsByLength.test.ts
+++ removeStringsByLength.test.ts
@@ -10,14 +10,4 @@ describe("removeStringsByLength", () => {
     const list = ["Hola", "viva", "el furbo", "me encanta"];
     expect(removeStringsByLength(list, 5)).toStrictEqual(["Hola", "viva"]);
   });
-
-  it("returns empty if every string length is larger than the number", () => {
-    const list = ["Un sabio dijo una vez", "'No entiendo nada'"];
-    expect(removeStringsByLength(list, 3)).toStrictEqual([]);
-  });
-
-  it("does not remove anything if every string length is shorter or equal than the number", () => {
-    const list = ["que", "so"];
-    expect(removeStringsByLength(list, 4)).toStrictEqual(["que", "so"]);
-  });
 });
