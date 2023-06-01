8898eb901d281ca4ee05b56942596b7958b081ef
Luis Corales <luisaugustocorales@hotmail.com>
2023-05-31 22:12:43 -0500

    Fix

--- removeStringsByLength.test.ts
+++ removeStringsByLength.test.ts
@@ -6,8 +6,8 @@ describe("removeStringsByLength", () => {
     expect(removeStringsByLength(list, 1)).toStrictEqual([]);
   });
 
-  it("removes the 'el furbo' off the list when passing 5", () => {
-    const list = ["Hola", "viva", "el furbo"];
+  it("removes the long words off the list when passing 5", () => {
+    const list = ["Hola", "viva", "el furbo", "me encanta"];
     expect(removeStringsByLength(list, 5)).toStrictEqual(["Hola", "viva"]);
   });
 
