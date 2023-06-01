b2b9f5d7396e09cdc60c9d6f73d00f38320a3f35
Luis Corales <luisaugustocorales@hotmail.com>
2023-05-31 22:02:48 -0500

    fix

--- removeStringsByLength.test.ts
+++ removeStringsByLength.test.ts
@@ -1,23 +1,23 @@
 import { removeStringsByLength } from "./removeStringsByLength";
 
 describe("removeStringsByLength", () => {
-  it("should return empty if list if empty", () => {
+  it("returns empty if list if empty", () => {
     const list = [];
     expect(removeStringsByLength(list, 1)).toStrictEqual([]);
   });
 
-  it("should remove the 'el furbo' off the list when passing 5", () => {
+  it("removes the 'el furbo' off the list when passing 5", () => {
     const list = ["Hola", "viva", "el furbo"];
     expect(removeStringsByLength(list, 5)).toStrictEqual(["Hola", "viva"]);
   });
 
-  // it("should return empty if every string length is larger than the number", () => {
-  //   const list = ["Un sabio dijo una vez", "'No entiendo nada'"];
-  //   expect(removeStringsByLength(list, 3)).toStrictEqual([]);
-  // });
+  it("returns empty if every string length is larger than the number", () => {
+    const list = ["Un sabio dijo una vez", "'No entiendo nada'"];
+    expect(removeStringsByLength(list, 3)).toStrictEqual([]);
+  });
 
-  // it("should not remove anything if every string length is shorter or equal than the number", () => {
-  //   const list = ["que", "so"];
-  //   expect(removeStringsByLength(list, 4)).toStrictEqual(["que", "so"]);
-  // });
+  it("does not remove anything if every string length is shorter or equal than the number", () => {
+    const list = ["que", "so"];
+    expect(removeStringsByLength(list, 4)).toStrictEqual(["que", "so"]);
+  });
 });
--- removeStringsByLength.ts
+++ removeStringsByLength.ts
@@ -1,32 +1,3 @@
 export const removeStringsByLength = (list: string[], length: number) => {
-  for (let i = 0; i < list.length; i++) {
-    if (list[i].length >= length) {
-      const index = list.indexOf(list[i]);
-      list.splice(index, 1);
-    }
-  }
-
-  return list;
+  return list.filter((item) => item.length <= length);
 };
-
-// export const removeStringsByLength = (list: string[], length: number) => {
-//   const newList: string[] = [];
-
-//   for (let i = 0; i < list.length; i++) {
-//     if (list[i].length <= length) {
-//       newList.push(list[i]);
-//     }
-//   }
-
-//   return newList;
-// };
-
-// export const removeStringsByLength = (list: string[], length: number) => {
-//   const newList = list.filter((item) => item.length <= length);
-
-//   return newList;
-// };
-
-// export const removeStringsByLength = (list: string[], length: number) => {
-//   return list.filter((item) => item.length <= length);
-// };
