4677cb1db2a2fa73e7a1fdadf81b8b1d779a3c47
Luis Corales <luisaugustocorales@hotmail.com>
2023-05-31 22:07:09 -0500

    first step

--- removeStringsByLength.test.ts
+++ removeStringsByLength.test.ts
@@ -6,11 +6,6 @@ describe("removeStringsByLength", () => {
     expect(removeStringsByLength(list, 1)).toStrictEqual([]);
   });
 
-  it("removes the 'el furbo' off the list when passing 5", () => {
-    const list = ["Hola", "viva", "el furbo"];
-    expect(removeStringsByLength(list, 5)).toStrictEqual(["Hola", "viva"]);
-  });
-
   it("returns empty if every string length is larger than the number", () => {
     const list = ["Un sabio dijo una vez", "'No entiendo nada'"];
     expect(removeStringsByLength(list, 3)).toStrictEqual([]);
@@ -19,5 +14,10 @@ describe("removeStringsByLength", () => {
   it("does not remove anything if every string length is shorter or equal than the number", () => {
     const list = ["que", "so"];
     expect(removeStringsByLength(list, 4)).toStrictEqual(["que", "so"]);
+  });
+
+  it("removes the 'el furbo' off the list when passing 5", () => {
+    const list = ["Hola", "viva", "el furbo"];
+    expect(removeStringsByLength(list, 5)).toStrictEqual(["Hola", "viva"]);
   });
 });
--- removeStringsByLength.ts
+++ removeStringsByLength.ts
@@ -1,3 +1,10 @@
 export const removeStringsByLength = (list: string[], length: number) => {
-  return list.filter((item) => item.length <= length);
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].length >= length) {
+      const index = list.indexOf(list[i]);
+      list.splice(index, 1);
+    }
+  }
+
+  return list;
 };
